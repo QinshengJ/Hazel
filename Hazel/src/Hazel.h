b5790770c2542c835b851bf052daeb0a82ed1b48
QinshengJ <2502581321@qq.com>
2024-06-30 19:24:46 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include<stdio.h>
+// Hazel application 使用
+#include"Hazel/Application.h"  
+
+//-------------入口-----------------
+#include"Hazel/EntryPoint.h"
+//---------------------------------
---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,16 @@
+#pragma once
+#include"core.h"
+namespace Hazel {
+
+class HAZEL_API Application
+{
+public:
+	Application() ;
+	virtual ~Application() ;
+	void Run();
+};
+// 在客户端中定义
+Application* CreateApplication();
+
+}
+

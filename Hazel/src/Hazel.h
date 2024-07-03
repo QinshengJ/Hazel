af8b203e66ac28839eb9afc1a8120f68359fdbd2
QinshengJ <2502581321@qq.com>
2024-07-03 12:56:15 +0800

    Update Hazel.h
    clear（）

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -1,9 +1,1 @@
-#pragma once
 
-#include<stdio.h>
-// Hazel application 使用
-#include"Hazel/Application.h"  
-
-//-------------入口-----------------
-#include"Hazel/EntryPoint.h"
-//---------------------------------
